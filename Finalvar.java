de7e363a548b3f7395d8502b07d452349f5c18c6
Kirthik <104076913+kirthik22@users.noreply.github.com>
2022-06-02 08:20:55 +0530

    Add files via upload

---
+++ Finalvar.java
@@ -0,0 +1,15 @@
+
+public class Finalvar
+{
+		final int a=20;
+		void disp()
+		{
+			a=4;
+			System.out.println(a);
+		}
+		public static void main(String[] args)
+		{
+			Finalvar v=new Finalvar();
+			v.disp();
+		}
+	}
